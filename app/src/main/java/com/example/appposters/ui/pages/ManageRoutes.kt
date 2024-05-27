a5f87aca28cc5091ad1afd7997388fd52fb9d77b
joao <joao.militao@magnumgroup.com.br>
2024-05-27 20:56:59 -0300

    add navigation animated

--- app/src/main/java/com/example/appposters/ui/pages/ManageRoutes.kt
+++ app/src/main/java/com/example/appposters/ui/pages/ManageRoutes.kt
@@ -1,5 +1,7 @@
 package com.example.appposters.ui.pages
 
+import androidx.compose.animation.*
+import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.compositionLocalOf
@@ -17,6 +19,23 @@ val LocalNavController = compositionLocalOf<NavHostController> {
     error("No LocalNavController provided")
 }
 
+fun scaleIntoContainer(
+): EnterTransition {
+    return scaleIn(
+        animationSpec = tween(220, delayMillis = 90),
+        initialScale =1.1f
+    ) + fadeIn(animationSpec = tween(220, delayMillis = 90))
+}
+
+fun scaleOutOfContainer(
+): ExitTransition {
+    return scaleOut(
+        animationSpec = tween(
+            durationMillis = 220,
+            delayMillis = 90
+        ), targetScale = 1.1f
+    ) + fadeOut(tween(delayMillis = 90))
+}
 
 @Composable
 fun ManageRoutes() {
@@ -24,9 +43,18 @@ fun ManageRoutes() {
     val navController = rememberNavController()
     CompositionLocalProvider(LocalNavController provides navController) {
         NavHost(navController = navController, startDestination = Routes.HOME.name) {
-            composable(Routes.HOME.name) { HomePage() }
+            composable(Routes.HOME.name, exitTransition = {
+                scaleOutOfContainer()
+            }, enterTransition = {
+                scaleIntoContainer()
+            }) { HomePage() }
             composable(
                 Routes.DETAILS.setParams("{data}"),
+                exitTransition = {
+                    scaleOutOfContainer()
+                }, enterTransition = {
+                    scaleIntoContainer()
+                }
             ) { entry ->
                 val data = entry.arguments?.getString("data") ?: ""
                 DetailsPage(post = PostModel.fromJson(data))
